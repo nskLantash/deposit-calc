35a961c544918462631afb7d05c87b3144ac541e
Lantash <nsklantash@gmail.com>
2019-03-01 20:02:32 +0700

    Add data-input by user

--- main.c
+++ main.c
@@ -3,5 +3,12 @@
 
 int main()
 {
+	int amount, term, f;
+	
+	printf("Enter the amount of money to deposit (10000 or more): ");
+	scanf("%d", &amount);
+	printf("Enter deposit life (from 1 to 365 days): ");
+	scanf("%d", &term);
+	
 	return 0;
 }
