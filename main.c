8112fc5505f85bd50f88a88d67c376fa1283a0d4
Lantash <nsklantash@gmail.com>
2019-03-01 20:05:56 +0700

    Add data-check function

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int data_check(int amount, int term);
+
 int main()
 {
 	int amount, term, f;
@@ -10,5 +12,21 @@ int main()
 	printf("Enter deposit life (from 1 to 365 days): ");
 	scanf("%d", &term);
 	
+	f = data_check(amount, term);
+	
 	return 0;
+}
+
+int data_check(int amount, int term)
+{
+    int result = 1;
+    if (amount < 10000) {
+		printf("You are trying to deposite too little money\n");
+		result = 0;
+	}
+	if (term <= 0 || term > 365) {
+		printf("The term is wrong\n");
+		result = 0;
+	}
+	return result;
 }
