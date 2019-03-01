c63b26e710496fc215c4a02f3659ed31ed1044eb
Lantash <nsklantash@gmail.com>
2019-03-01 21:48:55 +0700

    Update main.c to include calculation function

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 int data_check(int amount, int term);
+double calculate_deposit(int amount, int term);
 
 int main()
 {
@@ -29,4 +30,36 @@ int data_check(int amount, int term)
 		result = 0;
 	}
 	return result;
+}
+
+double calculate_deposit(int amount, int term)
+{
+	int I;
+	double sum;
+	if (term <= 30) { 
+	    I = -10;
+	} else if (term <= 120) {
+		if (amount <= 100000) {
+			I = 2;
+		} else {
+			I = 3;
+		}
+	} else if (term <= 240) {
+		if (amount <= 100000) {
+			I = 6;
+		} else {
+			I = 8;
+		}
+	} else {
+		if (amount <= 100000) {
+			I = 12;
+		}
+		else {
+			I = 15;
+		}
+	}
+	
+	sum = (amount * I * term / 365.) / 100.;
+	sum += amount;
+	return sum;
 }
