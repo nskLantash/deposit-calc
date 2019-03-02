a4a1336e077ec4d94170911cf334b47a1fc8ab16
Lantash <nsklantash@gmail.com>
2019-03-02 21:32:18 +0700

    Add tests for data check

--- test/validation_test.c
+++ test/validation_test.c
@@ -7,3 +7,15 @@ CTEST(suite1, test1) {
 	int expected = 0;
 	ASSERT_EQUAL(real, expected);
 }
+
+CTEST(suite1, test2) {
+    int real = data_check(9000, 210);
+	int expected = 0;
+	ASSERT_EQUAL(expected, real);
+}
+
+CTEST(suite2, test3) {
+    int real = data_check(100000, 365);
+	int expected = 1;
+	ASSERT_EQUAL(expected, real);
+}
